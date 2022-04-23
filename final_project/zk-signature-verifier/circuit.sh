9abb79770e2b76751763dd70658bfd051ea7d9cb
Elias Tazartes <elias.tazartes@gmail.com>
2022-04-23 17:45:52 +0200

    added v0

---
+++ final_project/zk-signature-verifier/circuit.sh
@@ -0,0 +1,60 @@
+### This command compiles the circuit to get a system of arithmetic equations representing it.
+
+# --r1cs:
+## The --r1cs contains the Rank-1 Constraint System (R1CS) in binary format. With circom, you design your own circuits with your own constraints, and the compiler outputs the R1CS representation that you will need for your zero-knowledge proof.
+
+# --wasm:
+## Generates a directory [name]_js where the .wasm file lives. These files are used to generate the witness, i.e., the set of signals that satisfy the circuit.
+
+# --sym:
+## Generates the file [name].sym. These symbols file are used to debugging/printing the constraint system in an annotated mode.
+
+# --c:
+## Generates the directory [name]_cpp. Contains the equivalent of the wasm directory used to generate the witness of the circuit.
+
+circom ./circuits/eddsasignaturesverifier.circom --r1cs --wasm --sym --c
+
+
+### Then, we need to generate a witness: a set of signals that satisfy the circuit. We'll need an inputs JSON file that contains all the circuit's inputs.
+
+# Logic/programmatic logic files: generate_witness.js, merkle.wasm 
+# Input file: input.json
+# Output file: witness.wtns 
+
+node eddsasignaturesverifier_js/generate_witness.js eddsasignaturesverifier_js/eddsasignaturesverifier.wasm input.json witness.wtns
+
+
+# Intermediary remark: After compiling the circuit and running the witness calculator with an appropriate input, we will have a file with extension .wtns that contains all the computed signals and, a file with extension .r1cs that contains the constraints describing the circuit. Both files will be used to create our proof.
+
+
+### We use Groth16 here. It is a zk-SNARK protocol. To use it, one needs to generate a trusted setup. As per the paper by cryptographic researchers, we need a common source of entropy to generate the proof. The trusted setup generation is equivalent to some ceremony where people agree to some source of randomness. It is considered a potential point of failure as any actor should not know the seed to the randomness used by all participants (unlikely in the real world, but important to know). 
+
+# Phase 1 of the trusted setup generation is called the powers of tau. It is independent of the circuit itself. Note that one should choose a power of tau high enough to run phase 2, otherwise will get an error of the from "X > 2^n".
+
+snarkjs powersoftau new bn128 14 pot14_0000.ptau -v
+
+snarkjs powersoftau contribute pot14_0000.ptau pot14_0001.ptau --name="First contribution" -v
+
+# Phase 2 is circuit-specific and is used to generate a .zkey file. This file contains proving and verification keys.
+
+snarkjs powersoftau prepare phase2 pot14_0001.ptau pot14_final.ptau -v
+
+snarkjs groth16 setup eddsasignaturesverifier.r1cs pot14_final.ptau eddsasignaturesverifier_0000.zkey
+
+snarkjs zkey contribute eddsasignaturesverifier_0000.zkey eddsasignaturesverifier_0001.zkey --name="1st Contributor Name" -v
+
+snarkjs zkey export verificationkey eddsasignaturesverifier_0001.zkey verification_key.json
+
+### Generate a proof. To compute the proof, you need a witness and a trusted setup for your circuit.
+
+snarkjs groth16 prove eddsasignaturesverifier_0001.zkey witness.wtns proof.json public.json
+
+### You're done ! You can now verify the proof from the CLI by running :
+
+snarkjs groth16 verify verification_key.json public.json proof.json
+
+# Or by verifying it from a smart contract :
+
+snarkjs zkey export solidityverifier eddsasignaturesverifier_0001.zkey verifier.sol
+
+
--- final_project/zk-signature-verifier/circuits/eddsasignaturesverifier.circom
+++ final_project/zk-signature-verifier/circuits/eddsasignaturesverifier.circom
@@ -1,44 +1,59 @@
 pragma circom 2.0.0;
 include "../circomlib/circuits/eddsaposeidon.circom";
 include "../circomlib/circuits/poseidon.circom";
+include "../circomlib/circuits/comparators.circom";
 
-template EdDSASignaturesVerifier(n) {
+template EdDSASignaturesVerifier(nInputs) {
     signal input signatureThreshold;
 
-    signal input Ax[n];
-    signal input Ay[n];
+    signal input Ax[nInputs];
+    signal input Ay[nInputs];
 
-    signal input S[n];
-    signal input R8x[n];
-    signal input R8y[n];
+    signal input S[nInputs];
+    signal input R8x[nInputs];
+    signal input R8y[nInputs];
 
     signal input message;
 
+    signal input isEnabled[nInputs];
+
     signal output hashedPubkeys;
     signal output validThreshold;
+    signal output messageHash;
+
+    messageHash <== message;
 
     var validSignatures = 0;
 
-    for (int i = 0; i < n; i++) {
-        if (S[i] != 0) {
-            component verifier = EdDSAPoseidonVerifier();
-            verifier.enabled <== 1;
-            verifier.Ax <== Ax[i];
-            verifier.Ay <== Ay[i];
-            verifier.S <== S[i];
-            verifier.R8x <== R8x[i];
-            verifier.R8y <== R8y[i];
-            verifier.M <== message;
-            validSignatures += 1;
-        }
-    }
+    component poseidonHashedPubkeys = Poseidon(nInputs);
+    component verifiers[nInputs];
+    for (var sig_idx = 0; sig_idx < nInputs; sig_idx++) {
+        // Checking the signature using circomlib EdDSAPoseidonVerifier() template
+        verifiers[sig_idx] = EdDSAPoseidonVerifier();
+        // If equal to 1, verifier.enabled will try to recover the signature. If 0, sig verification will be skipped.
+        // This enables the circuit to skip signers who did not sign.
+        // Checking that isEnabled[sig_idx] is either 0 or 1.
+        isEnabled[sig_idx]*(1-isEnabled[sig_idx]) === 0;
+        verifiers[sig_idx].enabled <== isEnabled[sig_idx];
 
-    component poseidonHashedPubkeys = Poseidon(n);
-    for (int i = 0; i < n; i++) {
-        poseidonHashedPubkeys.inputs[i] <== Ax[i];
+        verifiers[sig_idx].Ax <== Ax[sig_idx];
+        verifiers[sig_idx].Ay <== Ay[sig_idx];
+        verifiers[sig_idx].S <== S[sig_idx];
+        verifiers[sig_idx].R8x <== R8x[sig_idx];
+        verifiers[sig_idx].R8y <== R8y[sig_idx];
+        verifiers[sig_idx].M <== message;
+
+        validSignatures += 1;
+        // Storing the pubkeys' hash one by one and for it to be outputed to the smart contract to check for consistency and coherence.
+        poseidonHashedPubkeys.inputs[sig_idx] <== Ax[sig_idx];
     }
 
     hashedPubkeys <== poseidonHashedPubkeys.out;
-    var isValidVote = validSignatures > signaturesThreshold ? 1 : 0;
-    validThreshold <== isValidVote;
+    component greaterEqThan = GreaterEqThan(32);
+    greaterEqThan.in[0] <== validSignatures;
+    greaterEqThan.in[1] <== signatureThreshold;
+    validThreshold <== greaterEqThan.out;
 }
+
+// Compile with max_inputs 100, fill with zeros where needed until you reach max capacity.
+component main = EdDSASignaturesVerifier(15);
